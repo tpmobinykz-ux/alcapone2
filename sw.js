63cf74b51fe0571f4c603dc1e0bb6890432108c5
tpmobinykz-ux <tpmobinykz@gmail.com>
2026-02-14 13:55:17 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'alcapone-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
