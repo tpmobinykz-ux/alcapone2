eefc3372f44c8e4e765cbbda8c1db3efa5ae8d57
tpmobinykz-ux <tpmobinykz@gmail.com>
2026-02-15 03:45:07 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
-const cacheName = 'alcapone-v2';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const auth = document.getElementById("auth");
+const lobby = document.getElementById("lobby");
+const coins = document.getElementById("coins");
+const gems = document.getElementById("gems");
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
-});
+function guestLogin() {
+  const guestData = {
+    coins: 100,
+    gems: 10
+  };
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+  // ذخیره وضعیت مهمان
+  localStorage.setItem("alcapone_guest", JSON.stringify(guestData));
+
+  // رفتن به لابی
+  auth.classList.add("hidden");
+  lobby.classList.remove("hidden");
+
+  coins.innerText = "🪙 " + guestData.coins;
+  gems.innerText = "💎 " + guestData.gems;
+}
